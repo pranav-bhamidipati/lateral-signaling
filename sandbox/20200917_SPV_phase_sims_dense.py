99d6ef2e9fa83ae93b15abf176c33291fc7bd413
Pranav Bhamidipati <pbhamidi@usc.edu>
2020-09-24 11:29:48 -0700

    latest SPV script, HPC version
    Former-commit-id: ab4ee3453f9d4947bb13f981b5108994ef6f25ab

--- sandbox/20200917_SPV_phase_sims_dense.py
+++ sandbox/20200917_SPV_phase_sims_dense.py
@@ -12,6 +12,7 @@ import sys
 import os
 import numpy as np
 import pandas as pd
+import time
 
 # from tqdm import tqdm
 # import numba
@@ -28,7 +29,7 @@ import voronoi_model.voronoi_model_periodic as avm
 ########################
 
 # Inputs
-to_dir = "/home/pbhamidi/data/2020-09-17_avm_phase_sims_dense/"
+to_dir = "/home/pbhamidi/data/2020-09-23_avm_phase_sims_dense/"
 # to_dir = "C:\\Users\\Pranav\\git\\evomorph\\scratch"
 
 ########################
@@ -44,13 +45,14 @@ param_space = np.meshgrid(
 )
 param_space = np.array(param_space).T.reshape(-1, 3)
 
-param_space = param_space[::1000]
+param_space = param_space[:20]
 
 ########################
 
 f = 200
 t0 = 0
 tmax = 2
+
 dt = 0.025
 n_t = int((tmax - t0) * f / dt) + 1  # calculates the n_t to get the desired dt
 
@@ -63,23 +65,25 @@ common_metadata = dict(f=f, t0=t0, tmax=tmax, dt=dt, n_t=n_t, k=k, J=J)
 if not os.path.exists(to_dir):
     os.mkdir(to_dir)
 
-cores = 2
+cores = 10
 # gen = pcounter((param_space.shape[0] // cores) + 1)
 
 # def count():
 #     return next(gen)
 
 def simulate(params, progress_bar=False, print_updates=False):
+            
     p, v, rep = params
     rep = int(rep)
     prefix = f"p0{p:.2f}_v0{v:.2e}_rep{rep}"
 
     vor2 = avm.Tissue()
-    vor2.make_init2(L=10, n_c=400)
+    vor2.make_init2(L=14, n_c=235)
 
     vor2.set_GRN_t_span(t0, tmax, n_t, scaling_factor=f);
     vor2.v0 = v
-    vor2.n_warmup_steps = int(150 / dt)
+#    vor2.n_warmup_steps = int(150 / dt)
+    vor2.n_warmup_steps = 1
 
     W = J * np.array([[1, 0], [0, 1]])
     vor2.set_interaction(W=W, pE=0)
@@ -92,13 +96,20 @@ def simulate(params, progress_bar=False, print_updates=False):
     vor2.a = vor2.A0 / 2
     vor2.k = k
     
+    start_time = time.time()
+    
     vor2.simulate(progress_bar=progress_bar, print_updates=print_updates);
     
+    end_time = time.time()
+    secs_elapsed = int(end_time) - int(start_time)
+    mins_elapsed = secs_elapsed / 60
+    it_per_sec = vor2.n_t / secs_elapsed
+    
     fname = os.path.abspath(os.path.join(to_dir, prefix))
-    np.save(fname, vor2.x_save, allow_pickle=False)
+    np.savez_compressed(fname, vor2.x_save)
     
 #     print(f"Thread {count()*100:.2f}% complete")
-    return prefix
+    return prefix, mins_elapsed, it_per_sec
 #     return prefix, [vor_to_D_eff(vor2, n) for n in (7, 19, 37)]
 
 from multiprocessing import Pool
@@ -121,10 +132,20 @@ for k, v in common_metadata.items():
 coords_fname = []
 for _, row in enumerate(metadata[["p0", "v0", "rep"]].values):
     p, v, rep = row
-    coords_fname.append(f"p0{p:.2f}_v0{v:.2e}_rep{rep}.npy")
+    coords_fname.append(f"p0{p:.2f}_v0{v:.2e}_rep{int(rep)}")
 metadata["coords_fname"] = coords_fname
+
+time_df = pd.DataFrame(dict(
+    coords_fname=[res[0] for res in results],
+    mins_elapsed=[res[1] for res in results],
+    it_per_sec=[res[2] for res in results],
+))
 
 metadata.to_csv(os.path.join(to_dir, "metadata.csv"))
 
+metadata = metadata.merge(time_df)
+metadata.to_csv(os.path.join(to_dir, "metadata_time.csv"))
+
 # print("COMPLETE!")
+
 
