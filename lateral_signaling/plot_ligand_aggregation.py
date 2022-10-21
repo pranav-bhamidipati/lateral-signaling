4416a77a1e0fdb02fbc19ee5daa1c20f45c0ee2a
pranav-bhamidipati <pbhamidi@usc.edu>
2022-10-21 13:40:47 -0700

    plot latest ligand aggregation data

--- lateral_signaling/plot_ligand_aggregation.py
+++ lateral_signaling/plot_ligand_aggregation.py
@@ -13,41 +13,36 @@ lsig.viz.default_rcParams()
 
 def main(
     data_csv=lsig.data_dir.joinpath("aggregation/ligand_aggregation_data.csv"),
-    stats_method: Literal["bootstrap", "statsannotation"] = "bootstrap",
-    bs_medians_csv=lsig.anaysis_dir.joinpath(
-        "ligand_aggregation_bootstrap_medians.csv"
-    ),
+    figsize=(3, 3),
     save_dir=lsig.plot_dir,
     save=False,
     dpi=300,
     fmt="png",
 ):
-    df = pd.read_csv(data_csv)
-    df["Density"] = pd.Categorical(df["Density"], ordered=True)
-    df["Diameter_um"] = 2 * lsig.area_to_radius(df.Area_um2.values)
 
-    x = "Density"
-    y = "Diameter_um"
     order = ["1x", "2x", "4x"]
 
-    ax = sns.swarmplot(data=df, x=x, y=y, order=order)
-    ax.spines["top"].set_visible(False)
-    ax.spines["right"].set_visible(False)
+    df = pd.read_csv(data_csv)
+    df["Density"] = pd.Categorical(df["Density"], ordered=True, categories=order)
+
+    # df["Diameter_um"] = 2 * lsig.area_to_radius(df.Area_um2.values)
+    # x = "Density"
+    # y = "Diameter_um"
+
+    x = "Density"
+    y = "Area"
+
+    fig = plt.figure(figsize=figsize)
+
+    ax = sns.swarmplot(data=df, x=x, y=y, order=order, s=3)
 
     pairs = list(combinations(order, 2))
     annotator = Annotator(ax, pairs, data=df, x=x, y=y, order=order)
 
-    if stats_method == "statannotation":
-        annotator.configure(test="Mann-Whitney", text_format="star", loc="outside")
-        annotator.apply_and_annotate()
+    annotator.configure(test="Mann-Whitney", text_format="star", loc="outside")
+    annotator.apply_and_annotate()
 
-    elif stats_method == "bootstrap":
-        bs_medians_df = pd.read_csv(bs_medians_csv)
-
-        ## Get p-values for each pair
-
-        ## Draw using Annotator
-
+    sns.despine()
     plt.tight_layout()
 
     if save:
@@ -58,6 +53,6 @@ def main(
 
 if __name__ == "__main__":
     main(
-        save_dir=lsig.temp_plot_dir,
+        # save_dir=lsig.temp_plot_dir,
         save=True,
     )
